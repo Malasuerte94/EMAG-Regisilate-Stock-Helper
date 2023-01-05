22d35ff9a52bab52ccedd4b43615fb3d8cf7b0bb
Malasuerte94 <ene.catalin.94@gmail.com>
2023-01-05 16:43:38 +0200

    check more an format script

--- js/background.js
+++ js/background.js
@@ -1,204 +1,198 @@
 chrome.runtime.onInstalled.addListener((reason) => {
   if (reason === chrome.runtime.OnInstalledReason.INSTALL) {
-    console.log("Installed");
+    chrome.storage.local.set({ timeLoop: 1, searchProduct: ''});
   }
 });
 
 chrome.storage.onChanged.addListener(function (changes, namespace) {
   for (let [key, { oldValue, newValue }] of Object.entries(changes)) {
-    cancelAlarm();
-    check();
-  }
+        cancelAlarm();
+        check();
+    }
 });
 
 function check() {
-  chrome.storage.local.get(null, (items) => {
-    var activated = Boolean(items.isActive);
-    if (items.timeLoop >= 1 && activated) {
-      chrome.alarms.create("loopSearch", {
-        periodInMinutes: Number(items.timeLoop),
-      });
-      console.log("Start");
-    } else {
-      cancelAlarm();
-      console.log("Loop to short / Not Started");
-    }
-  });
+    chrome.storage.local.get(null, async (items) => {
+        var activated = Boolean(items.isActive);
+        var searchProduct = items.searchProduct;
+        if (items.timeLoop >= 1 && activated && searchProduct) {
+            await chrome.alarms.create('loopSearch', {
+                periodInMinutes: Number(items.timeLoop),
+            });
+            console.log('Start');
+        } else {
+            cancelAlarm();
+        }
+    });
 }
 
 chrome.alarms.onAlarm.addListener(function (alarm) {
-  if (alarm.name == "loopSearch") {
-    chrome.storage.local.get(null, (dbItems) => {
-      console.log("Search...");
-      if (!dbItems.searchUrl && !dbItems.searchProduct) {
-        console.log("Nu ai căutari salvate!");
-        return;
-      }
+    if (alarm.name == 'loopSearch') {
+        chrome.storage.local.get(null, (dbItems) => {
+            console.log('Search...');
+            if (!dbItems.searchUrl && !dbItems.searchProduct) {
+                console.log('Nu ai căutari salvate!');
+                return;
+            }
 
-      const url = dbItems.searchUrl + "/" + dbItems.searchProduct + "/c";
+            const url = dbItems.searchUrl + '/' + dbItems.searchProduct + '/c';
 
-      fetch(url)
-        .then((response) => response.text())
-        .then((text) => dothings(text))
-        .catch((error) => console.log(error));
+            fetch(url)
+                .then((response) => response.text())
+                .then((text) => dothings(text))
+                .catch((error) => console.log(error));
 
-      function extractSummary(content) {
-        var rx = /\EM\.listingGlobals\.items = \[(.*)}}]/g;
-        return content.match(rx);
-      }
+            function extractSummary(content) {
+                var rx = /\EM\.listingGlobals\.items = \[(.*)}}]/g;
+                return content.match(rx);
+            }
 
-      function dothings(values) {
-        var items = extractSummary(values)[0].replace(
-          "EM.listingGlobals.items = ",
-          ""
-        );
+            function dothings(values) {
+                var items = extractSummary(values)[0].replace('EM.listingGlobals.items = ', '');
 
-        items = JSON.parse(items);
-        var countItems = Object.keys(items).length;
+                items = JSON.parse(items);
+                var countItems = Object.keys(items).length;
 
-        if (dbItems.itemsArray) {
-          var newProducts = getDifference(items, dbItems.itemsArray);
-          if (newProducts.length >= 1) {
-            var newProductsCount = newProducts.length;
-            console.log("- new products:" + newProductsCount);
+                if (dbItems.itemsArray) {
+                    var newProducts = getDifference(items, dbItems.itemsArray);
+                    if (newProducts.length >= 1) {
+                        var newProductsCount = newProducts.length;
+                        console.log('- new products:' + newProductsCount);
 
-            var newProduct = {
-              type: "basic",
-              title: newProductsCount + " PRODUSE NOI",
-              message:
-                "Au aparut " +
-                newProductsCount +
-                " produse noi pentru căutarea ta!",
-              iconUrl: "/img/icon48.png",
-            };
-            chrome.notifications.create("", newProduct);
-            console.log("- found items: " + countItems);
-            let date = new Date().toLocaleString();
+                        var newProduct = {
+                            type: 'basic',
+                            title: newProductsCount + ' PRODUSE NOI',
+                            message: 'Au aparut ' + newProductsCount + ' produse noi pentru căutarea ta!',
+                            iconUrl: '/img/icon48.png',
+                        };
+                        chrome.notifications.create('', newProduct);
+                        console.log('- found items: ' + countItems);
+                        let date = new Date().toLocaleString();
 
-            chrome.storage.local.set({
-              items: countItems,
-              itemsArray: items,
-              newProducts: newProducts,
-              newProductsDate: date
-            });
-          } else {
-            console.log("- no new products");
-          }
-        } else {
-          chrome.storage.local.set({
-            items: countItems,
-            itemsArray: items,
-            newProducts: null,
-            newProductsDate: null
-          });
-        }
-      }
-    });
-  }
+                        chrome.storage.local.set({
+                            items: countItems,
+                            itemsArray: items,
+                            newProducts: newProducts,
+                            newProductsDate: date,
+                        });
+                    } else {
+                        console.log('- no new products');
+                    }
+                } else {
+                    chrome.storage.local.set({
+                        items: countItems,
+                        itemsArray: items,
+                        newProducts: [],
+                        newProductsDate: null,
+                    });
+                }
+            }
+        });
+    }
 });
 
 chrome.notifications.onClicked.addListener(function (notificationId, byUser) {
-  chrome.storage.local.get(null, (items) => {
-    var url = buildLink(items.searchUrl + "/" + items.searchProduct + "/c");
-    if (!url) {
-      url = items.searchUrl + "/" + items.searchProduct + "/c";
-    }
-    chrome.tabs.create({
-      url: buildLink(items.searchUrl + "/" + items.searchProduct + "/c"),
+    chrome.storage.local.get(null, (items) => {
+        var url = buildLink(items.searchUrl + '/' + items.searchProduct + '/c');
+        if (!url) {
+            url = items.searchUrl + '/' + items.searchProduct + '/c';
+        }
+        chrome.tabs.create({
+            url: buildLink(items.searchUrl + '/' + items.searchProduct + '/c'),
+        });
     });
-  });
 });
 
 function getDifference(array1, array2) {
-  return array1.filter((object1) => {
-    return !array2.some((object2) => {
-      return object1.id === object2.id;
+    return array1.filter((object1) => {
+        return !array2.some((object2) => {
+            return object1.id === object2.id;
+        });
     });
-  });
 }
 
-function cancelAlarm() {
-  console.log("Cancel Looper");
-  chrome.alarms.clear("loopSearch");
+async function cancelAlarm() {
+    await chrome.alarms.clear('loopSearch');
+    console.log('Cancel Looper');
 }
 
 //affiliate build
 var domains = [
-  "emag.ro",
-  "emag.page.link",
-  "emag.page.link",
-  "marketplace-leads.emag.ro",
-  "stage1.emag.ro",
-  "m.emag.ro",
+    'emag.ro',
+    'emag.page.link',
+    'emag.page.link',
+    'marketplace-leads.emag.ro',
+    'stage1.emag.ro',
+    'm.emag.ro',
 ];
-var domainsHash = ["9", "9", "9", "9", "9", "9"];
-var currentDomainHash = "";
-var websiteId = "LYp";
-var affiliateHash = "mUu";
-var trackingServerDomain = "l.profitshare.ro";
+var domainsHash = ['9', '9', '9', '9', '9', '9'];
+var currentDomainHash = '';
+var websiteId = 'LYp';
+var affiliateHash = 'mUu';
+var trackingServerDomain = 'l.profitshare.ro';
 var generateLinkOnLoad = true;
-var sid = "searcher";
+var sid = 'searcher';
 var debug = false;
 var whitelistPaths = [
-  /profitshare\.(ro|com|bg|hu|pl)\/l\/(.*)/,
-  /profitshare\.(ro|com|bg|hu|pl)\/lps\/(.*)/,
-  /w\.profitshare\.(ro|com|bg|hu|pl)/,
-  /campanii\.emag\.(ro|bg|hu|pl)/,
-  /campaigns\.emag\.(ro|bg|hu|pl)/,
+    /profitshare\.(ro|com|bg|hu|pl)\/l\/(.*)/,
+    /profitshare\.(ro|com|bg|hu|pl)\/lps\/(.*)/,
+    /w\.profitshare\.(ro|com|bg|hu|pl)/,
+    /campanii\.emag\.(ro|bg|hu|pl)/,
+    /campaigns\.emag\.(ro|bg|hu|pl)/,
 ];
 
 function getDomainAndProtocol(url) {
-  var matches = url.match(/^(https?)\:\/\/([^\/?#]+)(?:[\/?#]|$)/i);
-  var protocol = matches && matches[1];
-  var domain = matches && matches[2];
-  return { domain: domain, protocol: protocol };
+    var matches = url.match(/^(https?)\:\/\/([^\/?#]+)(?:[\/?#]|$)/i);
+    var protocol = matches && matches[1];
+    var domain = matches && matches[2];
+    return { domain: domain, protocol: protocol };
 }
 
 function matchesParentDomain(domain) {
-  currentDomainHash = "";
-  for (var i = 0; i < domains.length; i++) {
-    var suffix = "." + domains[i];
-    if (domain.indexOf(suffix, domain.length - suffix.length) !== -1) {
-      currentDomainHash = domainsHash[i];
-      return true;
+    currentDomainHash = '';
+    for (var i = 0; i < domains.length; i++) {
+        var suffix = '.' + domains[i];
+        if (domain.indexOf(suffix, domain.length - suffix.length) !== -1) {
+            currentDomainHash = domainsHash[i];
+            return true;
+        }
     }
-  }
-  return false;
+    return false;
 }
 
 function buildLink(url) {
-  var start = new Date().getTime();
-  var buildedUrl = "";
-  var domainAndProtocol = getDomainAndProtocol(url);
-  if (!domainAndProtocol.domain) return;
+    var start = new Date().getTime();
+    var buildedUrl = '';
+    var domainAndProtocol = getDomainAndProtocol(url);
+    if (!domainAndProtocol.domain) return;
 
-  var domainInLowerCase = domainAndProtocol.domain.toLowerCase();
+    var domainInLowerCase = domainAndProtocol.domain.toLowerCase();
 
-  for (let index in whitelistPaths) {
-    if (url.toLowerCase().match(whitelistPaths[index])) {
-      return;
+    for (let index in whitelistPaths) {
+        if (url.toLowerCase().match(whitelistPaths[index])) {
+            return;
+        }
     }
-  }
 
-  var keyDomain = domains.indexOf(domainInLowerCase);
-  if (keyDomain >= 0) {
-    currentDomainHash = domainsHash[keyDomain];
-  }
-  if (keyDomain >= 0 || matchesParentDomain(domainInLowerCase)) {
-    var extraParams = "?redirect=" + encodeURIComponent(url);
-    if (sid) {
-      extraParams += "&hash=" + sid;
+    var keyDomain = domains.indexOf(domainInLowerCase);
+    if (keyDomain >= 0) {
+        currentDomainHash = domainsHash[keyDomain];
     }
-    buildedUrl =
-      domainAndProtocol.protocol +
-      "://" +
-      trackingServerDomain +
-      "/lps/" +
-      currentDomainHash +
-      "/" +
-      affiliateHash +
-      "/" +
-      extraParams;
-  }
-  return buildedUrl;
+    if (keyDomain >= 0 || matchesParentDomain(domainInLowerCase)) {
+        var extraParams = '?redirect=' + encodeURIComponent(url);
+        if (sid) {
+            extraParams += '&hash=' + sid;
+        }
+        buildedUrl =
+            domainAndProtocol.protocol +
+            '://' +
+            trackingServerDomain +
+            '/lps/' +
+            currentDomainHash +
+            '/' +
+            affiliateHash +
+            '/' +
+            extraParams;
+    }
+    return buildedUrl;
 }
--- js/options.js
+++ js/options.js
@@ -1,50 +1,50 @@
 //set data to seee
 chrome.storage.local.get(null, (items) => {
-    console.log(items);
     optionsForm.timeLoop.value = String(items.timeLoop);
     optionsForm.isActive.checked = Boolean(items.isActive);
-    document.getElementById("searchFor").innerHTML = String(
-      items.searchProduct
-    );
-    notificationDiv(
-      String(items.newProducts.length),
-      items.manualUrl,
-      items.newProductsDate
-    );
+    document.getElementById('searchFor').innerHTML = String(items.searchProduct);
+
+    if (!items.searchUrl || items.timeLoop < 1 || !items.searchProduct) {
+        optionsForm.isActive.checked = false;
+        optionsForm.isActive.disabled = true;
+    }
+
+    if (items.newProducts) {
+        notificationDiv(String(items.newProducts.length), items.manualUrl, items.newProductsDate);
+    }
 });
 
 //start stop
-optionsForm.isActive.addEventListener("change", (event) => {
+optionsForm.isActive.addEventListener('change', (event) => {
     var isActive = event.target.checked;
     chrome.storage.local.set({ isActive: isActive });
     console.log(isActive);
 });
 
 //save data
-optionsForm.saveData.addEventListener("click", (event) => {
+optionsForm.saveData.addEventListener('click', (event) => {
     var timeLoop = optionsForm.timeLoop.value;
     chrome.storage.local.set({ timeLoop: timeLoop });
-    console.log("Data saved");
+    console.log('Data saved');
 });
-
 
 function notificationDiv(produs, url, date) {
     if (produs && url && date) {
-      let cautare = document.createElement("div");
-      cautare.classList.add("last_notify");
-      cautare.innerHTML = "<span class='hour'>" + date + "</span> Am gasit " + produs + " produse noi! | ";
+        let cautare = document.createElement('div');
+        cautare.classList.add('last_notify');
+        cautare.innerHTML = "<span class='hour'>" + date + '</span> Am gasit ' + produs + ' produse noi! | ';
 
-      let buttonLink = document.createElement("button");
-      buttonLink.classList.add("buttonLink");
-      buttonLink.innerHTML = "Verifica";
+        let buttonLink = document.createElement('button');
+        buttonLink.classList.add('buttonLink');
+        buttonLink.innerHTML = 'Verifica';
 
-      cautare.appendChild(buttonLink);
-      document.getElementById("productNotification").appendChild(cautare);
+        cautare.appendChild(buttonLink);
+        document.getElementById('productNotification').appendChild(cautare);
 
-      cautare.addEventListener("click", function () {
-        chrome.tabs.create({
-          url: url,
+        cautare.addEventListener('click', function () {
+            chrome.tabs.create({
+                url: url,
+            });
         });
-      });
     }
 }
